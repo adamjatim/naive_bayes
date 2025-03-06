23b56434290377b9812c4de03ee3a6d1b7c18f8a
adamjatim <adamrahmatilahi@gmail.com>
2025-03-06 15:22:31 +0700

    update 2 files and delete 2 files

--- resources/views/pages/profile/index.blade.php
+++ resources/views/pages/profile/index.blade.php
@@ -1,42 +1,150 @@
 @extends('layout.app')
 
-@section('title', 'Dataset')
+@section('title', 'Profile')
 
 @section('content')
 
-    <div class="flex flex-col flex-grow gap-0 md:m-6 md:mx-10 m-4">
-        <div class="p-4 rounded-md rounded-b-none border">
-            <h1 class="text-2xl font-bold text-gray-800">Profile</h1>
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Profile
+            </h2>
         </div>
-        <div class="p-2.5 rounded-md rounded-t-none border">
-            <div class="flex flex-col md:flex-row md:gap-6 w-full">
-                <div class="w-1/3 p-2">
-                    <h1 class="text-md font-medium text-gray-700">Profile Information</h1>
-                    <h3 class="text-sm font-normal text-gray-600">Update your account's profile information and email address.
-                    </h3>
+    </header>
+
+    <main class="bg-gray-100">
+        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="md:grid md:grid-cols-3 md:gap-6">
+                <div class="md:col-span-1 flex justify-between">
+                    <div class="px-4 sm:px-0">
+                        <h3 class="text-lg font-medium text-gray-900">Profile Information</h3>
+
+                        <p class="mt-1 text-sm text-gray-600">
+                            Update your account's profile information and email address.
+                        </p>
+                    </div>
+
+                    <div class="px-4 sm:px-0">
+
+                    </div>
                 </div>
-                <div class="w-2/3 p-2 border rounded-md flex flex-col">
-                    <div class="w-4/6 flex flex-col gap-4">
-                        <div class="flex flex-col">
-                            <label class="block font-medium text-sm text-gray-700" for="name">Name</label>
-                            <input
-                                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
-                                id="name" type="text">
+
+                <div class="mt-5 md:mt-0 md:col-span-2">
+                    <form >
+                        <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
+                            <div class="grid grid-cols-6 gap-6">
+
+                                {{-- Name --}}
+                                <div class="col-span-6 sm:col-span-4">
+                                    <label class="block font-medium text-sm text-gray-700" for="name">
+                                        Name
+                                    </label>
+                                    <input
+                                        class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
+                                        id="name" type="text" autocomplete="name" value="{{ Auth::user()->name }}">
+                                </div>
+
+                                {{-- Email --}}
+                                <div class="col-span-6 sm:col-span-4">
+                                    <label class="block font-medium text-sm text-gray-700" for="email">
+                                        Email
+                                    </label>
+                                    <input
+                                        class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
+                                        id="email" type="email" value="{{ Auth::user()->email }}">
+
+                                </div>
+                            </div>
                         </div>
-                        <div class="flex flex-col">
-                            <label class="block font-medium text-sm text-gray-700" for="email">Email</label>
-                            <input
-                                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
-                                id="email" type="text">
+
+                        <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                            <div style="display: none;" class="text-sm text-gray-600 mr-3">
+                                Saved.
+                            </div>
+
+                            <button type="submit"
+                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                                Save
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
+            <div class="hidden sm:block">
+                <div class="py-8">
+                    <div class="border-t border-gray-200"></div>
+                </div>
+            </div>
+
+            <div class="mt-10 sm:mt-0">
+                <div class="md:grid md:grid-cols-3 md:gap-6">
+                    <div class="md:col-span-1 flex justify-between">
+                        <div class="px-4 sm:px-0">
+                            <h3 class="text-lg font-medium text-gray-900">Update Password</h3>
+
+                            <p class="mt-1 text-sm text-gray-600">
+                                Ensure your account is using a long, random password to stay secure.
+                            </p>
+                        </div>
+
+                        <div class="px-4 sm:px-0">
+
                         </div>
                     </div>
-                    <div class="w-2/6 ms-auto ">
-                        <button type="submit">Save</button>
+
+                    <div class="mt-5 md:mt-0 md:col-span-2">
+                        <form>
+                            <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
+                                <div class="grid grid-cols-6 gap-6">
+                                    <div class="col-span-6 sm:col-span-4">
+                                        <label class="block font-medium text-sm text-gray-700" for="current_password">
+                                            Current Password
+                                        </label>
+                                        <input
+                                            class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
+                                            id="current_password" type="password" autocomplete="current-password">
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-4">
+                                        <label class="block font-medium text-sm text-gray-700" for="password">
+                                            New Password
+                                        </label>
+                                        <input
+                                            class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
+                                            id="password" type="password" autocomplete="new-password">
+                                    </div>
+
+                                    <div class="col-span-6 sm:col-span-4">
+                                        <label class="block font-medium text-sm text-gray-700" for="password_confirmation">
+                                            Confirm Password
+                                        </label>
+                                        <input
+                                            class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm mt-1 block w-full"
+                                            id="password_confirmation" type="password" autocomplete="new-password">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div
+                                class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                                <div style="display: none;" class="text-sm text-gray-600 mr-3">
+                                    Saved.
+                                </div>
+
+                                <button type="submit"
+                                    class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                                    Save
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
 
             </div>
+
         </div>
-    </div>
+    </main>
 
 @endsection
