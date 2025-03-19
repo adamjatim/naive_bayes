39c11fc85cdcd894cb2e73068076842187d12b14
adamjatim <adamrahmatilahi@gmail.com>
2025-03-19 16:21:08 +0700

    update: resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,2 +1,7 @@
 import './bootstrap';
 import 'flowbite';
+// import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2/dist/sweetalert2.js'
+// import 'sweetalert2/src/sweetalert2.scss'
+
+// window.Sweetalert2 = Swal;
