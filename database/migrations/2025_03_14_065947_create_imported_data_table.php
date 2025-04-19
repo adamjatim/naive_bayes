0fc352ac61798fb20555cf47a2b05dfebccee512
adamjatim <adamrahmatilahi@gmail.com>
2025-04-19 15:09:38 +0700

    update 1 file and copy 1 file

--- database/migrations/2025_03_14_065947_create_imported_data_table.php
+++ database/migrations/2025_03_14_065947_create_imported_data_table.php
@@ -13,9 +13,33 @@ return new class extends Migration
     {
         Schema::create('imported_data', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('dataset_id')->nullable();
-            $table->json('row_data');
-            $table->string('user_email'); // User yang terakhir mengupdate
+
+            // Data Demografi
+            $table->string('rw', 10);
+            $table->enum('kriteria', ['lansia', 'anak_sekolah', 'usia_produktif']);
+            $table->integer('usia');
+            $table->string('nama', 100);
+
+            // Atribut Pendukung
+            $table->string('tanggungan_kepala_keluarga', 20); // Sedikit/Sedang/Banyak
+            $table->enum('lansia', ['ada', 'tidak_ada']);
+            $table->enum('anak_wajib_sekolah', ['ada', 'tidak_ada']);
+            $table->string('penghasilan_kepala_keluarga', 20); // Rendah/Sedang/Tinggi
+            $table->string('status_bpjs', 50);
+            $table->string('tipe_kendaraan', 30);
+            $table->string('sumber_air', 30);
+            $table->string('tipe_jamban', 30);
+            $table->string('status_kepemilikan_bangunan', 30);
+            $table->string('bahan_lantai', 30);
+            $table->string('bahan_dinding', 30);
+            $table->string('kategori_luas_bangunan', 20); // Kecil/Sedang/Besar
+
+            // Label Target
+            $table->enum('keterangan', ['penerima', 'bukan_penerima']);
+
+            $table->string('user_id'); // User yang terakhir mengupdate
+            $table->string('file_name')->nullable();
+            $table->bigInteger('file_size')->nullable();
             $table->timestamps();
         });
     }
